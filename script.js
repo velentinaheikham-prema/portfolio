447f038e8440a94fdb6d13faf2a0d53dad3db9bc
Heikham Velentina <24BCAR197@gcu.edu.in>
2026-06-13 16:07:13 +0530

    updating portfolio webpage

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function showMessage() {
-  document.getElementById("message").innerText =
-    "Thank you for reaching out! I will contact you soon 😊";
+  const messageEl = document.getElementById("message");
+  messageEl.innerText = "Thank you for reaching out! I will contact you soon 😊";
+  messageEl.classList.add("show");
+  
+  // Hide message after 5 seconds
+  setTimeout(() => {
+    messageEl.classList.remove("show");
+  }, 5000);
 }
